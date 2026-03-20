f53dc7d5a6180ca88856e74d87ece89c64a90501
YangZhicheng <yangzhicheng@YangZhichengdeMacBook-Air.local>
2026-03-20 14:13:08 +0800

    fix: update api prefix

--- hsp_payment_service/transport/http/router.py
+++ hsp_payment_service/transport/http/router.py
@@ -10,7 +10,7 @@ from hsp_payment_service.transport.http.schemas import (
 
 
 def build_router(echo_service: EchoService) -> APIRouter:
-    router = APIRouter(prefix="/api/v1", tags=["echo"])
+    router = APIRouter(prefix="/api/payment/v1", tags=["echo"])
 
     @router.post(
         "/echo",
